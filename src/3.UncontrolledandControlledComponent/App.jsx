7b3a9dd95de39c40a49b819c1a9aec28e9a82116
ahmad17-fe <ahmads1704@gmail.com>
2021-11-07 13:24:16 +0700

    add controlled and uncontrolled modal

--- src/3.UncontrolledandControlledComponent/App.jsx
+++ src/3.UncontrolledandControlledComponent/App.jsx
@@ -1,20 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import UncontrolledForm from "./UncontrolledForm";
 import ControlledForm from "./ControlledForm";
+import UncontrolledModal from "./UncontrolledModal";
+import ControlledModal from "./ControlledModal";
 
 const App = () => {
+  const [shouldShowModal, setShouldModal] = useState(false);
+
   return (
     <>
       <fieldset>
-        <legend>Uncontrolled Form</legend>
-        <UncontrolledForm />
+        <legend>Form</legend>
+        <fieldset>
+          <legend>Uncontrolled Form</legend>
+          <UncontrolledForm />
+        </fieldset>
+        <br />
+        <hr />
+        <br />
+        <fieldset>
+          <legend>Controlled Form</legend>
+          <ControlledForm />
+        </fieldset>
       </fieldset>
       <br />
       <hr />
       <br />
       <fieldset>
-        <legend>Controlled Form</legend>
-        <ControlledForm />
+        <legend>Modal</legend>
+        <fieldset>
+          <legend>Uncontrolled Modal</legend>
+          <UncontrolledModal>
+            <h1>Uncontrolled Modal</h1>
+          </UncontrolledModal>
+        </fieldset>
+        <br />
+        <hr />
+        <br />
+        <fieldset>
+          <legend>Controlled Modal</legend>
+          <button onClick={() => setShouldModal(!shouldShowModal)}>
+            {shouldShowModal ? "Hide Modal" : "Show Modal"}
+          </button>
+          <ControlledModal
+            shouldShow={shouldShowModal}
+            onRequestClose={() => setShouldModal(!shouldShowModal)}
+          >
+            <h1>Controlled Modal</h1>
+          </ControlledModal>
+        </fieldset>
       </fieldset>
     </>
   );
